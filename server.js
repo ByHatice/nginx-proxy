e7a9dcd92989d2f29823b0498ea7ded7f3573e49
Hatice Celik <hatice.celik@iths.se>
2025-02-22 13:28:08 +0100

    Configure Nginx to proxy requests to multiple Node.js backend servers (ports 3001-3003) with load balancing. Add copy of nginx.conf for reference

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const path = require('path');
+// Import the necessary modules from the Node.js standard library
+const express = require('express'); // Import the Express framework
+const path = require('path'); // Import the path module for file path handling
+
+// Initialize an Express application
 const app = express();
-const port = 3000;
+const port = 3000; 
 
-const replicaApp = process.env.APP_NAME
+// Fetch the application name from the environment variable and allows different instances of the app to run with different names
+const replicaApp = process.env.APP_NAME; 
 
+// Serve static files (like images) from the 'images' folder
+// This means that any request to '/images' will serve files from the 'images' directory in the app's root folder
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+
+// Define the main route for the application
 app.use('/', (req, res) => {
+    // Serve the 'index.html' file when the root URL is accessed
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${replicaApp}`);
+
+    // Log which replica of the app is serving the request (useful for debugging or multi-instance setup)
+    console.log(`Request served by ${replicaApp}`);  
 });
 
+// Start the Express server and listen on the specified port
 app.listen(port, () => {
-    console.log(`${replicaApp} Is listening on port ${port}`);
+    
+    // Log a message indicating the app has started and is ready to accept requests
+    console.log(`${replicaApp} is listening on port ${port}`);
 });
